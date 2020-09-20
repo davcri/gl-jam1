9a512b4a336573b0d63b1e442491c49a72a74bc3
Davide Cristini <davcri91@gmail.com>
2020-09-20 23:31:48 +0200

    Try stuff

--- entities/warriors/warrior.gd
+++ entities/warriors/warrior.gd
@@ -1,17 +1,33 @@
 class_name Warrior
 extends KinematicBody2D
+tool
+
+enum GFX_ID {
+	WAR_1,
+	WAR_2,
+	WAR_3,
+	WAR_4,
+}
+
+export(GFX_ID) var warrior_sprite_id = GFX_ID.WAR_1 setget set_warrior_sprite_id
+onready var sprite = $Sprite
 
 
 func _ready() -> void:
-	pass # Replace with function body.
+	_change_texture(warrior_sprite_id)
 
 
-func _process(delta: float) -> void:
-	if Input.is_key_pressed(KEY_1):
-		position.x -= 1
-	if Input.is_key_pressed(KEY_2):
-		position.x += 1
-		
+func _change_texture(sprite_id):
+	if sprite == null:
+		return
+	sprite.region_rect.position.x = 16 * sprite_id
+
+
+func set_warrior_sprite_id(new_id):
+	warrior_sprite_id = new_id
+	_change_texture(warrior_sprite_id)
+	
+	
 
 func show_wanted_item(wanted_item):
 	pass 
